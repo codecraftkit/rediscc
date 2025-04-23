62fa4ab8af272e104fc9c94412aaf4b039ae8174
Jose G Nieto <jose.nieto@noblenex.us>
2025-04-23 12:23:54 -0500

    v2.0.0

--- rediscc.go
+++ rediscc.go
@@ -1,22 +1,60 @@
 package rediscc
 
 import (
+	"context"
 	"fmt"
 	"github.com/redis/go-redis/v9"
+	"time"
 )
 
-func Connect(redisUri string, dbNumber string, dataStore *RedisDataStore) error {
+func Connect(redisUri string, dbNumber string) (*RedisDataStore, error) {
 	redisUrl := fmt.Sprintf("%s/%s", redisUri, dbNumber)
 	opt, err := redis.ParseURL(redisUrl)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	client := redis.NewClient(opt)
 
 	fmt.Printf("You successfully connected to Redis: %s\n", redisUrl)
 
-	dataStore.Client = client
+	redisDataStore := &RedisDataStore{
+		client: client,
+		Debug:  true,
+	}
 
-	return nil
+	return redisDataStore, nil
+}
+
+type RedisDataStore struct {
+	client *redis.Client
+	Debug  bool
+}
+
+func (redisDataStore *RedisDataStore) Publish(ctx context.Context, channel string, payload interface{}) error {
+	if redisDataStore.Debug {
+		fmt.Println("Publish", channel, payload)
+	}
+	return redisDataStore.client.Publish(ctx, channel, payload).Err()
+}
+
+func (redisDataStore *RedisDataStore) Get(ctx context.Context, key string) *redis.StringCmd {
+	if redisDataStore.Debug {
+		fmt.Println("Get", key)
+	}
+	return redisDataStore.client.Get(ctx, key)
+}
+
+func (redisDataStore *RedisDataStore) Set(ctx context.Context, key string, value interface{}, expiration time.Duration) error {
+	if redisDataStore.Debug {
+		fmt.Println("Set", key, value, expiration)
+	}
+	return redisDataStore.client.Set(ctx, key, value, expiration).Err()
+}
+
+func (redisDataStore *RedisDataStore) SetWithExpiration(ctx context.Context, key string, value interface{}, expiration time.Duration) error {
+	if redisDataStore.Debug {
+		fmt.Println("SetWithExpiration", key, value, expiration)
+	}
+	return redisDataStore.client.Set(ctx, key, value, expiration).Err()
 }
--- structs.go
+++
@@ -1,9 +0,0 @@
-package rediscc
-
-import (
-	"github.com/redis/go-redis/v9"
-)
-
-type RedisDataStore struct {
-	Client *redis.Client
-}
